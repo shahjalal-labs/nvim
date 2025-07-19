6b9dd3a0102f315159b730cfc65800ca3ec38610
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-19 22:44:17 +0600

    improve: Updated Git/autoPush2.lua

--- lua/sj/core/custom/Git/autoPush2.lua
+++ lua/sj/core/custom/Git/autoPush2.lua
@@ -1,4 +1,4 @@
--- keep this for manual command
+--p: git push  1 LOC allowed
 vim.api.nvim_create_user_command("GitSmartPush", function()
 	local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
 	intelligentGitPushOneLocAvailable(git_root)
@@ -9,4 +9,3 @@ vim.keymap.set("n", "<leader>gh", function()
 	local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
 	intelligentGitPushOneLocAvailable(git_root)
 end, { desc = "1 LOC allowed Manual intelligent Git push" })
---u
